13122f203ac28a6791d1cf7521858bc8fade682e
Mikael Mortensen <mikaem@math.uio.no>
2023-08-18 10:13:28 +0200

    Adding mesh_function.py

---
+++ projects/mesh_function.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+def mesh_function(f, t):
+    pass
+
+def func(t):
+    pass
+
+def test_mesh_function():
+    t = np.array([1, 2, 3, 4])
+    f = np.array([np.exp(-1), np.exp(-2), np.exp(-3), np.exp(-12)])
+    fun = mesh_function(func, t)
+    assert np.allclose(fun, f)
