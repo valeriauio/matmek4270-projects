947f057e2c15bd27ea011759837cc59bdd49d017
Mikael Mortensen <mikaem@math.uio.no>
2023-08-18 10:58:18 +0200

    Update differentiate.py

--- projects/differentiate.py
+++ projects/differentiate.py
@@ -4,5 +4,13 @@ import numpy as np
 def differentiate(u, dt):
     pass
 
+def differentiate_vector(u, dt):
+    pass
+
 def test_differentiate():
-    assert False
+    t = np.linspace(0, 1, 10)
+    dt = t[1] - t[0]
+    u = t**2
+    du1 = differentiate(u, dt)
+    du2 = differentiate_vector(u, dt)
+    assert np.allclose(du1, du2)
